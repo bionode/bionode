51a3933877064cbe72e426ddcf5c9d9011996afe
Bruno Vieira <mail@bmpvieira.com>
2014-04-04 18:19:32 +0100

    Adds new methods, such as findLongestOpenReadingFrame
    Adds getReadingFrames that takes a sequence and returns all six possible frames
    Adds getOpenReadingFrames that takes a sequence and returns ORFs but only for that sense
    Adds getAllOpenReadingFrames that takes a sequence and returns an array with six arrays containing all the ORFs for all the frames
    Adds findLongestOpenReadingFrame that only returns the longest from getAllOpenReadingFrames
    Adds translate from dna/rna to aa
    Adds removeIntrons, that takes a sequence and an array of exons ranges
    Fixes transcribe to get the coding RNA strand

--- test/bionode.js
+++ test/bionode.js
@@ -5,7 +5,7 @@ var data = require('./data')
 require('mocha')
 
 describe("check sequence type", function() {
-  it("should return strings 'dna' or 'rna' using sequence. Doesn't check for mixing errors", function(done) {
+  it("should return strings 'dna' or 'rna' from sequence. Doesn't check for mixing errors", function(done) {
     bionode.checkSequenceType(data.dnaSequence).should.equal('dna')
     bionode.checkSequenceType(data.rnaSequence).should.equal('rna')
     done()
@@ -21,14 +21,14 @@ describe("create complement base", function() {
 
 
 describe("reverse", function() {
-  it("should return a reversed string using sequence", function(done) {
+  it("should return a reversed string from sequence", function(done) {
     bionode.reverse(data.dnaSequence).should.equal(data.dnaReverseSequence)
     done()
   })
 })
 
 describe("complement", function() {
-  it("should return complement string using sequence", function(done) {
+  it("should return complement string from sequence", function(done) {
     bionode.complement(data.dnaSequence).should.equal(data.dnaComplementSequence)
     bionode.complement(data.rnaSequence).should.equal(data.rnaComplementSequence)
     done()
@@ -36,7 +36,7 @@ describe("complement", function() {
 })
 
 describe("reverse complement", function() {
-  it("should return a reverse complemented string using sequence", function(done) {
+  it("should return a reverse complemented string from sequence", function(done) {
     bionode.reverseComplement(data.dnaSequence).should.equal(data.dnaReverseComplementSequence)
     done()
   })
@@ -44,14 +44,14 @@ describe("reverse complement", function() {
 
 describe("transcribe base", function() {
   it("should return transcribe of base", function(done) {
-    bionode.getTranscribeBase('A').should.equal('U')
-    bionode.getTranscribeBase('a').should.equal('u')
-    bionode.getTranscribeBase('T').should.equal('A')
-    bionode.getTranscribeBase('t').should.equal('a')
-    bionode.getTranscribeBase('C').should.equal('G')
-    bionode.getTranscribeBase('c').should.equal('g')
-    bionode.getTranscribeBase('G').should.equal('C')
-    bionode.getTranscribeBase('g').should.equal('c')
+    bionode.getTranscribedBase('A').should.equal('U')
+    bionode.getTranscribedBase('a').should.equal('u')
+    bionode.getTranscribedBase('T').should.equal('A')
+    bionode.getTranscribedBase('t').should.equal('a')
+    bionode.getTranscribedBase('C').should.equal('G')
+    bionode.getTranscribedBase('c').should.equal('g')
+    bionode.getTranscribedBase('G').should.equal('C')
+    bionode.getTranscribedBase('g').should.equal('c')
     done()
   })
 })
@@ -59,19 +59,20 @@ describe("transcribe base", function() {
 describe("transcribe", function() {
   it("should return rna string from dna string and vice versa", function(done) {
     bionode.transcribe(data.dnaSequence).should.equal(data.rnaSequence)
+    bionode.transcribe(data.rnaSequence).should.equal(data.dnaSequence)
     done()
   })
 })
 
 describe("transcribe with exons", function() {
-  it("should return rna string from dna string and remove exons provided", function(done) {
-    bionode.transcribe(data.simDNASequence, data.simExonsRanges).should.equal(data.simRNASequence)
+  it("should return rna string from dna string and remove introns if exons positions provided", function(done) {
+    bionode.transcribe(data.simDNASequence, data.simExonsRanges).should.equal(data.simRNASequenceNoIntrons)
     done()
   })
 })
 
 describe("reverse exons", function() {
-  it("should return right exons coordinates reversed using array of exons and reference length", function(done) {
+  it("should return right exons coordinates reversed from array of exons and reference length", function(done) {
     var exonsRangesReversed = bionode.reverseExons(data.exonsRanges, data.length)
     exonsRangesReversed.should.eql(data.exonsRangesReversed)
 
@@ -88,7 +89,7 @@ describe("reverse exons", function() {
 })
 
 describe("find non canonical splices", function() {
-  it("should return array with splices sites using reference and exons ranges", function(done) {
+  it("should return array with splices sites from reference and exons ranges", function(done) {
     bionode.findNonCanonicalSplices(data.dnaSequence, data.exonsRanges).should.eql([19272])
     done()
   })
@@ -98,6 +99,22 @@ describe("check canonical translation start site", function() {
   it("should return true if provided sequence starts with ATG/AUG", function(done) {
     bionode.checkCanonicalTranslationStartSite(data.simDNASequenceCanonicalTranslation).should.be.true
     bionode.checkCanonicalTranslationStartSite(data.simDNASequenceNonCanonicalTranslation).should.be.false
+    done()
+  })
+})
+
+describe("find longest open reading frame", function() {
+  it("should return the dna or rna string of the longest reading frame", function(done) {
+    bionode.findLongestOpenReadingFrame(data.simDNASequence).should.equal(data.simDNALongestReadingFrame)
+    bionode.findLongestOpenReadingFrame(data.simRNASequence).should.equal(data.simRNALongestReadingFrame)
+    done()
+  })
+})
+
+describe("translate to amino acids", function() {
+  it("should return the amino acids string from dna or rna string", function(done) {
+    bionode.translate(data.simDNASequence).should.equal(data.simAASequence)
+    bionode.translate(data.simRNASequence).should.equal(data.simAASequence)
     done()
   })
 })
