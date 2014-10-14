5c249a25bf2ccb9156a63af41476bd26f21abf3a
Bruno Vieira <mail@bmpvieira.com>
2014-10-14 07:06:26 +0100

    Add CLI name and fix spawn stdin

--- cli.js
+++ cli.js
@@ -2,9 +2,13 @@
 var bio = require('./')
 var spawn = require('child_process').spawn
 
-var module = process.argv[2]
-var cli = spawn(__dirname + '/node_modules/bionode-' + module + '/cli.js', process.argv.slice(3))
+var component = process.argv[2]
+var path = __dirname + '/node_modules/bionode-' + component + '/cli.js'
+var args = process.argv.slice(3)
+var cli = spawn(path, args)
 
 cli.stdout.pipe(process.stdout)
-
 cli.stderr.pipe(process.stderr)
+
+if (!process.stdin.isTTY) { process.stdin.pipe(cli.stdin) }
+else { cli.stdin.end() }
