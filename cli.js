2634665d4aaee55e2378598e1a263e72ef682ff0
Max Ogden <max@maxogden.com>
2014-10-21 17:10:42 +0900

    dont open stdin by default

--- cli.js
+++ cli.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 var bio = require('./')
+var argv = require('minimist')(process.argv.slice(2))
 var spawn = require('child_process').spawn
 
-var component = process.argv[2]
+var component = argv._[0]
 var path = __dirname + '/node_modules/bionode-' + component + '/cli.js'
 var args = process.argv.slice(3)
 var cli = spawn(path, args)
@@ -10,5 +11,8 @@ var cli = spawn(path, args)
 cli.stdout.pipe(process.stdout)
 cli.stderr.pipe(process.stderr)
 
-if (!process.stdin.isTTY) { process.stdin.pipe(cli.stdin) }
-else { cli.stdin.end() }
+if (argv._[argv._.length - 1] === '-') {
+  process.stdin.pipe(cli.stdin)
+} else {
+  cli.stdin.end()
+}
